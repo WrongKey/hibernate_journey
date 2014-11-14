44d03e82bbaa0b2aefc5384749a7159bef573292
WrongKey <18280480654@163.com>
2014-11-14 11:07:48 +0800

    利用hibernate向数据库insert一个对象

--- hibernatech01/src/main/java/com/wrongkey/hibernate/HibernatePerson.java
+++ hibernatech01/src/main/java/com/wrongkey/hibernate/HibernatePerson.java
@@ -2,6 +2,7 @@ package com.wrongkey.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
 import org.hibernate.cfg.Configuration;
@@ -15,13 +16,38 @@ import org.hibernate.cfg.Configuration;
  */
 public class HibernatePerson {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().configure();
+
+        Person p1 = new Person();
+        p1.setId(4);
+        p1.setLastName("yanjun");
+        p1.setFirstName("Song");
+        p1.setAddress("guojiaqiao street");
+        p1.setCity("Chengdu");
+        //获取配置信息
+        Configuration configuration = new Configuration().configure("com.wrongkey.hibernate/hibernatech01.cfg.xml");
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
 
         StandardServiceRegistryImpl registry = (StandardServiceRegistryImpl) builder.build();
-
+        //获取SessionFactory
         SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+        //打开Session
         Session session = sessionFactory.openSession();
+        //开启事务Transaction
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            //sava对象p1
+            session.save(p1);
+            //提交事务
+            transaction.commit();
+        } catch (Exception e) {
+            //事务回滚
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            //关闭Session
+            session.close();
+        }
 
     }
 }
